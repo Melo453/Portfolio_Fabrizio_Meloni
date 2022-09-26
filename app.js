8171f0e9dc8b1f25eae56dfa98d9b39360e4a6b7
Fabrizio Meloni <fabrizio.meloni@accendoit.com>
2022-09-26 14:45:19 -0300

    .

--- app.js
+++ app.js
@@ -6,12 +6,6 @@ const path = require('path');
 app.set('port', (process.env.PORT || 2500));
 app.use(express.static(path.join(__dirname, 'public'))); 
 
-<<<<<<< HEAD
-=======
-/*Glide js*/ 
-app.use("/css", express.static(__dirname + "/node_modules/@glidejs/glide/dist/css"));
-app.use("/dist", express.static(__dirname + "/node_modules/@glidejs/glide/dist"));
->>>>>>> 978b073ceabfa18ddefae08d8ea37cc4fff46a00
 
 app.set("views", __dirname + '/views');
 app.set("view engine", "ejs");
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,25 +1,45 @@
-addEventListener('DOMContentLoaded', ()=>{
-    const header__btn = document.querySelector('.header__btn');
-    const header__quit = document.querySelector('.header__quit')
-    if (header__btn) {
-        header__btn.addEventListener('click', () =>{
-            const header__ul = document.querySelector('.header__ul');
-            header__ul.classList.toggle('show');
-            header__btn.classList.toggle('header__hidden');
-            header__btn.classList.remove('header__nohidden');
-            header__quit.classList.toggle('header__nohidden')
-            header__quit.classList.remove('header__hidden')
-        });
-    }
-    if(header__quit){
-        header__quit.addEventListener('click', () =>{
-            const header__ul = document.querySelector('.header__ul');
-            header__ul.classList.remove('show');
-            header__quit.classList.toggle('header__hidden')
-            header__quit.classList.remove('header__nohidden')
-            header__btn.classList.toggle('header__nohidden')
-            header__btn.classList.remove('header__hidden')
-        });
-    }
-});
+const list = document.querySelectorAll('.navigation__list');
 
+function activeLink(){
+    list.forEach((item) => item.classList.remove('active'));
+    this.classList.add('active');
+}
+
+list.forEach((item) => item.addEventListener('click', function(event){
+    event.preventDefault();
+}));
+
+list.forEach((item) => item.addEventListener('click', activeLink));
+
+
+const text = document.querySelector(".inicio__move");
+const textLoad = () =>{
+    setTimeout(() =>{
+        text.textContent = "Web developer";
+    },0);
+    setTimeout(() =>{
+        text.textContent = "Freelancer";
+    },4000);
+    setTimeout(() =>{
+        text.textContent = "Certified tech developer";
+    },8000);
+};
+
+
+textLoad();
+setInterval(textLoad,12000);
+
+document.querySelector(".menu--mobile").addEventListener("click", animateBars);
+
+var line1 = document.querySelector(".menu__bars1");
+var line2 = document.querySelector(".menu__bars2");
+var line3 = document.querySelector(".menu__bars3");
+var menu =  document.querySelector(".navigation--mobile__nav");
+
+function animateBars() {
+    line1.classList.toggle("activemenu__bars1");
+    line2.classList.toggle("activemenu__bars2");
+    line3.classList.toggle("activemenu__bars3");
+
+    menu.classList.toggle("active__menu")
+};
